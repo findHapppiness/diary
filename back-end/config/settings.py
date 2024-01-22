d8f8bb3dea8d66f230abeddd2a9416d3ba2d1ea0
Sanghyeop Lee <d0003@naver.com>
2024-01-22 19:52:08 +0900

    refactor: store credentials on environment variables #2

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
+
+# Reading .env file
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pv#w%uh1))=^+l%2$2-5ns2&9o^t8i!(+io(--&xhj*o)$5&5$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
